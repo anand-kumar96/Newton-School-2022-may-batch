ca1f67815108a4b319dfde66f4965fc83c78d69c
aryananand01 <106487247+aryananand01@users.noreply.github.com>
2022-06-24 13:18:16 +0530

    Create Print_the_input.java

---
+++ Basic programmings/Print_the_input.java
@@ -0,0 +1,39 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		System.out.println(a);
+                      // Your code here
+	}
+}
+/*
+Print the input!
+Easy
+
+Problem Statement
+Take an integer as input and print it.
+Input
+The first line contains integer as input.
+
+Constraints
+1 <= N <= 10
+Output
+Print the input integer in a single line
+Example
+Sample Input:-
+2
+
+Sample Output:-
+2
+
+Sample Input:-
+4
+
+Sample Output:-
+4
+*/
