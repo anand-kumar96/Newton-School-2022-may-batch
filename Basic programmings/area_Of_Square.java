915a1207c6369daea40d75fa9622ed6ab05f946b
aryananand01 <106487247+aryananand01@users.noreply.github.com>
2022-06-24 13:02:26 +0530

    Add files via upload

---
+++ Basic programmings/area_Of_Square.java
@@ -0,0 +1,10 @@
+package com.company.Newton_School.Basic_programming;
+import java.util.Scanner;
+public class area_Of_Square {
+public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+                int side =sc.nextInt();
+                int area= side*side;
+                System.out.println(area);
+            }
+        }
