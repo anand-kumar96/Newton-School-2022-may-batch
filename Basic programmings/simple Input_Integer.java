9f9a76e4811e2cfaf32e868872c4a3286211ef6e
aryananand01 <106487247+aryananand01@users.noreply.github.com>
2022-06-24 13:31:21 +0530

    Create simple Input_Integer.java

---
+++ Basic programmings/simple Input_Integer.java
@@ -0,0 +1,43 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.println(n);
+                      // Your code here
+	}
+}
+
+
+/*
+Simple Input- Integer
+Easy
+
+Time Limit: 2 sec
+Memory Limit: 128000 kB
+Problem Statement
+Given an integer N, your task is to take input of the given integer and print it.
+Input
+The input contains a single integer N.
+
+Constraints:-
+1 <= N <= 100
+Output
+Print the integer N.
+Example
+Sample Input:-
+3
+
+Sample Output:_
+3
+
+Sample Input:-
+56
+
+Sample Output:-
+56
+*/
