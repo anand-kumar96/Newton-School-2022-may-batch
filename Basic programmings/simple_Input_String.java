597e3820d0b1e2e67188512c7281bdad5221c6a0
aryananand01 <106487247+aryananand01@users.noreply.github.com>
2022-06-24 13:28:27 +0530

    Create simple_Input_String.java

---
+++ Basic programmings/simple_Input_String.java
@@ -0,0 +1,62 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+	public static void main (String[] args) {
+		Scanner sc= new Scanner(System.in);
+		String S=sc.nextLine();
+		System.out.println(S);
+                     
+	}
+}
+/*
+Simple Input- String
+Easy
+
+Time Limit: 2 sec
+Memory Limit: 128000 kB
+Problem Statement
+Given a string S, your task is to take input of the given string and print the same string.
+Input
+The input contains a single string S.
+Output
+Print the string S.
+Example
+Sample Input:-
+NewtonSchool
+
+Sample Output:-
+NewtonSchool
+
+Sample Input:-
+Hello
+
+Sample Output:-
+Hello
+Need Help? Go to Doubt Portal 
+
+Run
+Submit
+12345678910111213
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+	public static void main (String[] args) {
+		Scanner sc= new Scanner(System.in);
+		String S=sc.nextLine();
+		System.out.println(S);
+                     
+	}
+}
+Custom Input
+STDOUT
+STDERR
+Compile Output
+Message
+
+Run against demo code
