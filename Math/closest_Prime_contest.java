cccea5a3fc6145bb1ed33e6e3ff975615c1b5f88
aryananand01 <106487247+aryananand01@users.noreply.github.com>
2022-06-27 09:51:22 +0530

    Update closest_Prime_contest.java

--- Math/closest_Prime_contest.java
+++ Math/closest_Prime_contest.java
@@ -22,6 +22,11 @@ class Main {
          return;
      }
      for(int i=0;i<=n;i++){
+	     // or
+// 	      if(isprime(n)){
+//          System.out.println(n);
+//          break;
+//      }
          if(isprime(n-i)==true || isprime(n+i)==true ){
              if(isprime(n-i)){
                  System.out.println(n-i);
@@ -75,7 +80,46 @@ System.out.println(j);
 }
 }
  }
+// method 03
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
 
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static boolean isprime(int n){
+        if(n<=1){
+            return false;
+        }
+        for(int i=2;i<=(int)Math.sqrt(n);i++){
+            if(n%i==0)
+                return false;
+        }
+        return true;
+    }
+	public static void main (String[] args) {
+     Scanner sc=new Scanner(System.in);
+     int n=sc.nextInt();
+     if(isprime(n)){
+         System.out.println(n);
+         return;
+     }
+      int i=n ,j=n;
+       while(true){
+             if(isprime(i)){
+                 System.out.println(i);
+                 break;
+             }else  if(isprime(j)){
+                 System.out.println(j);
+                 break;
+         }
+         i--;
+         j++;
+
+     }
+
+	}
+}
 
 /*
 Closest Prime (Contest)
