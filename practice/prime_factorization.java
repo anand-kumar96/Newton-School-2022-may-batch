b8bc83925a295716c503ea9a4eb55cfb48f2ffb8
aryananand01 <106487247+aryananand01@users.noreply.github.com>
2022-06-27 13:32:21 +0530

    Add files via upload

---
+++ practice/prime_factorization.java
@@ -0,0 +1,19 @@
+package com.company.Newton_School.Basic_programming;
+import java.util.Scanner;
+public class prime_factorization {
+    public static void primeFactorization(int n) {
+        for (int i = 2; i <= n; i++) {
+            while (n % i == 0) {
+                    System.out.println(i);
+                n = n / i;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        primeFactorization(n);
+    }
+}
