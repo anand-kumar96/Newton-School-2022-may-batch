4b600c4d38c5433bf591ca152cabe9b95519df06
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-07-26 14:07:50 +0530

    Create Interesting Numbers.java

---
+++ Recursion/Interesting Numbers.java
@@ -0,0 +1,42 @@
+/*
+Interesting Numbers
+Medium
+
+Time Limit: 2 sec
+Memory Limit: 128000 kB
+Problem Statement
+Bobs love interesting numbers, According to him, a number is interesting if it follows the given conditions:
+The number is positive
+The digits of the number when considered from left to right, they form an alternate odd even sequence.
+A sequence is an alternate odd even sequence when the numbers present on even indices in the sequence are odd, 
+and the numbers present on the odd indices are even, where the sequence is having 1 based indexing.
+
+You are given the number K.
+
+Task
+Determine the Kth smallest interesting number.
+
+Note
+Answer should not contain any leading zeros.
+Input
+The first line of the input contains T i.e the number of testcases.
+Next T lines contains the value of K
+
+Constraints
+1 <= T <= 1e5
+1 <= K <= 1e5
+Output
+Print the required number.
+Example
+Sample Input 1:
+1
+3
+
+Sample Output 1:
+6
+
+Explanation
+The first three interesting numbers are [2, 4, 6]. So the 3rd smallest number is 6.
+*/
+// code
+
