661d945cc48ff1eaf1148609cbed36abd45ed980
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-07-26 13:57:52 +0530

    Create Multiplication.java

---
+++ Recursion/Multiplication.java
@@ -0,0 +1,35 @@
+/*
+Multiplication
+Easy
+
+Problem Statement
+Given two numbers m and n, multiply them using only "addition" operations.
+Input
+User Task:
+Since this will be a functional problem, you don't have to take input. You just have to complete the function Multiply() that takes the integer M and N as a parameter.
+
+Constraints:
+1 ≤ T ≤ 100
+0 ≤ M, N ≤ 100
+Output
+Return the product of N and M.
+Example
+Sample Input
+2
+2 3
+3 4
+
+Sample Output
+6
+12
+*/
+//code
+static int  Multiply_by_recursion(int M, int N) 
+    { 
+      if(N==0){
+        return 0;
+      }
+ int mul=M+Multiply_by_recursion(M, N-1); 
+ 
+ return mul;
+    }
