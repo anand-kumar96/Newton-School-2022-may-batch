10d84f2bb140e2c849cf0f361d95baf608a272ae
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-07-30 11:45:24 +0530

    Create Insert node at the head.java

---
+++ SingleLinkedList InClass/Insert node at the head.java
@@ -0,0 +1,19 @@
+/*
+class Node {
+    Node next;
+    int val;
+
+    Node(int val) {
+        this.val = val;
+        next = null;
+    }
+}
+*/
+
+public static Node addElement(Node head, int k) {
+Node temp=new Node(k);
+temp.next=head;
+head=temp;
+return head;
+
+}
