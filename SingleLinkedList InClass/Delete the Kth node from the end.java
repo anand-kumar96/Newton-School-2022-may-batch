e5c5964d753d39f841e4edc814c645d4e93e6867
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-07-31 13:29:27 +0530

    Update Delete the Kth node from the end.java

--- SingleLinkedList InClass/Delete the Kth node from the end.java
+++ SingleLinkedList InClass/Delete the Kth node from the end.java
@@ -31,3 +31,31 @@ return head;
 }
 
 // method 02
+/*
+class Node {
+    Node next;
+    int val;
+
+    Node(int val) {
+        this.val = val;
+        next = null;
+    }
+}
+*/
+ public static Node deleteElement(Node head, int k) {
+        Node dummy = new Node(-1);
+        dummy.next = head;
+        Node p1 = dummy;
+        Node p2 = dummy;
+        for (int i = 0; i < k; i++) {
+            p1 = p1.next;
+        }
+
+        while (p1.next != null) {
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        p2.next = p2.next.next;
+
+        return dummy.next;
+    }
