60b52607a6094675122419375fbda14c0ecbb28b
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-08-02 19:16:47 +0530

    Update Maximum difference array.java

--- one_D_array/Maximum difference array.java
+++ one_D_array/Maximum difference array.java
@@ -34,7 +34,37 @@ Sample Input:-
 Sample Output:-
 -1
 */
-// code
+// brute force
+
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int arr[]=new int[n];
+    for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+    }
+    int ans=-1;
+  int max=0;
+    for(int i=0;i<n-1;i++){
+     for(int j=i+1;j<n;j++){
+       if(arr[j]>arr[i]){
+           max=j-i;
+           ans=Math.max(ans,max);
+       }
+     }
+
+    }
+    System.out.println(ans);
+    }
+}
+
+// optimized apparoach
 import java.io.*; // for handling input/output
 import java.util.*; // contains Collections framework
 
