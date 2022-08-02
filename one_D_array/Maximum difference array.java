f135e34b400fa3bc56814e184f034f4af4ddb06e
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-08-02 19:43:03 +0530

    Update Maximum difference array.java

--- one_D_array/Maximum difference array.java
+++ one_D_array/Maximum difference array.java
@@ -63,6 +63,31 @@ class Main {
     System.out.println(ans);
     }
 }
+// another but tle
+class Main {
+    public static void main (String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int arr[]=new int[n];
+    for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+    }
+ int ans=-1;
+  int max=0;
+    for(int i=0;i<n-1;i++){
+        int j=i+1;
+        while(j<n){
+             if(arr[j]>arr[i]){
+            max=j-i;
+            ans=Math.max(ans,max);
+        }
+        j++;
+        }
+     }
+    System.out.println(ans);
+    }
+}
+
 
 // optimized apparoach
 import java.io.*; // for handling input/output
