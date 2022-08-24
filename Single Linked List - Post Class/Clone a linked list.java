243edac6f6bbdebd3acf2d9fa524ac26ce4a3177
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-08-24 14:47:16 +0530

    Update Clone a linked list.java

--- Single Linked List - Post Class/Clone a linked list.java
+++ Single Linked List - Post Class/Clone a linked list.java
@@ -14,29 +14,25 @@ class Node {
 */
 
 public static Node CloneList(Node head) {
-    Node curr=head;
-    while (curr!=null) {
-        Node temp=curr.next;
-        curr.next=new Node(curr.val);
-        curr.next.next=temp;
-        curr=temp;
-    }
-    curr=head;
-    while(curr!=null) {
-        if (curr.next!=null) {
-            curr.next.random=(curr.random!=null)?
-                    curr.random.next:curr.random;
-        }
-        curr=curr.next.next;
-    }
-    Node org=head,copy=head.next;
-    Node temp=copy;
-    while (org!=null) {
-        org.next=org.next.next;
-        copy.next=(copy.next != null) ?
-                 copy.next.next: copy.next;
-        org=org.next;
-        copy=copy.next;
-    }
-    return temp;
+Node curr=head;
+while(curr!=null){
+    Node temp=curr.next;
+    curr.next=new Node(curr.val);
+    curr.next.next=temp;
+    curr=temp;
+}
+curr=head;
+while(curr!=null){
+    curr.next.random=(curr.random!=null)?curr.random.next:curr.random;
+    curr=curr.next.next;
+}
+Node original=head,copy=head.next;
+Node temp=copy;
+while(original!=null && copy!=null){
+    original.next=original.next.next;
+    copy.next=(copy.next!=null)?copy.next.next:copy.next;
+    original=original.next;
+    copy=copy.next;
+}
+return temp;
 }
