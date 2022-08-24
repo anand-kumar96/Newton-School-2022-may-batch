62f99a1adc2c30c5dffaa04c846549c32cde3bbb
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-08-24 13:18:45 +0530

    Update Delete node without head pointer.java

--- Single Linked List - Post Class/Delete node without head pointer.java
+++ Single Linked List - Post Class/Delete node without head pointer.java
@@ -11,9 +11,13 @@ class Node {
 */
 
 public static void deleteNode(Node D) {
-    Node next=D.next;
-    Node temp=next.next;
-    D.val=next.val;
-    D.next=temp;
+// for this we need two node so that we can get d next 
+//and d next next node
+//2 - > 3 - > 4 - > 5  // D-->3
+Node nextNode=null;
+nextNode=D.next; // nextNode-->4
+ Node temp=nextNode.next; // temp-->5
+ D.val=nextNode.val; //D-->4
+ D.next=temp;//D.next->5 i.e 3 is deleted means D become next
 
 }
