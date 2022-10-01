d0f6a49a295cb920573341e8b11686bbb02bfc00
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-10-01 10:17:23 +0530

    Update Good Integer.java

--- Question of the Day/29 September/Good Integer.java
+++ Question of the Day/29 September/Good Integer.java
@@ -67,3 +67,81 @@ Sample Input 2:
 Sample Output 2:
 0
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// c++ solution
+#include <iostream>
+  #include <vector>
+   #include <algorithm>
+        using namespace std;
+        int main(){
+            int N; cin>>N;
+            vector<int>a(N),b(N);
+            for(int i=0;i<N;i++){
+                cin>>a[i];
+            }
+             for(int i=0;i<N;i++){
+                cin>>b[i];
+            }
+       sort(b.begin(),b.end());
+        vector<int>ans;
+        int x;
+        for(int i=0;i<N;i++){
+            if(i!=0 && b[i-1]==b[i]){
+                continue;
+            }
+            x=a[0]^b[i];
+            vector<int>c;
+             for(int j=0;j<N;j++){
+                 c.push_back(a[j]^x);
+                 }
+             sort(c.begin(),c.end());
+             int good=1;
+             for(int j=0;j<N;j++){
+                if(b[j]!=c[j]){
+                    good=0;
+                    break;
+                }
+             }
+             if(good){
+                 ans.push_back(x);
+             }
+        }
+         sort(ans.begin(),ans.end());
+         cout << ans.size() <<"\n";
+          for(int i=0;i<ans.size();i++){
+              cout << ans[i] <<"\n";
+          }
+        }
